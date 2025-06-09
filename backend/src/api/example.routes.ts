ff2c099eab5469b62efc30cf2df3dca89f434b93
OppaDev <jlobando2@espe.edu.ec>
2025-06-09 00:54:29 -0500

    conbinar 2 ramas y evitar conflictos eliminanado viejo backend v2.3 Leonardo

--- backend/src/api/example.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getExampleData } from '../controllers/example.controller';
-
-const router = Router();
-
-router.get('/', getExampleData);
-
-export default router;
