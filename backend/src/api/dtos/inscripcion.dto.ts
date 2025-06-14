a9a6419e74f0a6ecac88f9f5c5a619e8583addca
OppaDev <jlobando2@espe.edu.ec>
2025-06-14 00:12:27 -0500

    fix: resuelto Security Hotspots en en filename v2.5 Leonardo

---
+++ backend/src/api/dtos/inscripcion.dto.ts
@@ -0,0 +1,31 @@
+import {
+    IsNumber,
+    IsOptional,
+    IsBoolean
+} from "class-validator";
+
+export class CreateInscripcionDto {
+    @IsNumber({}, { message: "El ID del curso debe ser un número" })
+    idCurso!: number;
+
+    @IsNumber({}, { message: "El ID de la persona debe ser un número" })
+    idPersona!: number;
+
+    @IsNumber({}, { message: "El ID de la facturación debe ser un número" })
+    idFacturacion!: number;
+}
+
+export class InscripcionResponseDto {
+    estadoInscripcion!: string; 
+}
+
+export class UpdateInscripcionDto {
+    @IsOptional()
+    @IsNumber({}, { message: "El ID del descuento debe ser un número" })
+    idDescuento?: number;
+
+    
+    @IsOptional()
+    @IsBoolean({ message: "El estado de matrícula debe ser un booleano" })
+    matricula?: boolean;
+}
