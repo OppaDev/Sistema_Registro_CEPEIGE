20b0c385fc087fe456030befbc484386a98d6aad
OppaDev <jlobando2@espe.edu.ec>
2025-06-22 16:30:14 -0500

    test: Cponfiguracion de CI/CD con sonarqube v3.8 Leonardo

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -4,11 +4,10 @@ module.exports = {
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1', 
     },
-    // testMatch: ['**/__tests__/**/*.test.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'], // Patrón por defecto
-    testMatch: ['**/src/**/*.test.ts'], // Para encontrar los .test.ts dentro de src
-    collectCoverage: true, // Habilitar recolección de cobertura
-    coverageDirectory: 'coverage', // Directorio para los reportes de cobertura
-    coverageReporters: ['json', 'lcov', 'text', 'clover', 'cobertura'], // Formatos de reporte, 'cobertura' es útil para SonarQube
-    // setupFilesAfterEnv: ['./jest.setup.js'], // Si necesitas setup global (ej. para mocks de Prisma globales)
-    clearMocks: true, // Limpia mocks automáticamente antes de cada test
+   
+    testMatch: ['**/src/**/*.test.ts'], 
+    collectCoverage: true, 
+    coverageDirectory: 'coverage', 
+    coverageReporters: ['json', 'lcov', 'text', 'clover', 'cobertura'], 
+    clearMocks: true, 
 };
